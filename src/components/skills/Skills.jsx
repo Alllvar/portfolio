60652f57d29f6e323cb2c896e7cd282cf263ff11
csiworks-martyniukv <martyniukv@csiworks.net>
2026-01-27 13:01:15 +0200

    Fix skills section

--- src/components/skills/Skills.jsx
+++ src/components/skills/Skills.jsx
@@ -1,4 +1,3 @@
-// src/components/Skills.jsx
 import React from 'react';
 import './skills.scss';
 import htmlLogo from '../../assets/html5.svg';
@@ -27,66 +26,45 @@ import {
 } from 'react-icons/si';
 
 const skills = [
-    { icon: <img src={htmlLogo} alt="JavaScript" className="skills__img-icon" />, name: 'HTML 5' },
-    { icon: <img src={jsLogo} alt="JavaScript" className="skills__img-icon" />, name: 'JavaScript (ES6+)' },
-    { icon: <img src={tsLogo} alt="TypeScript" className="skills__img-icon" />, name: 'TypeScript' },
+    { icon: <img src={htmlLogo} alt="HTML5" />, name: 'HTML 5' },
+    { icon: <img src={jsLogo} alt="JavaScript" />, name: 'JavaScript (ES6+)' },
+    { icon: <img src={tsLogo} alt="TypeScript" />, name: 'TypeScript' },
     { icon: <SiReact />, name: 'React', color: '#61DAFB' },
     { icon: <SiRedux />, name: 'Redux', color: '#764ABC' },
-    { icon: <img src={reduxSaga} alt="Redux-Saga" className="skills__img-icon-saga" />, name: 'Redux-Saga' },
+    { icon: <img src={reduxSaga} alt="Redux-Saga" />, name: 'Redux-Saga' },
     { icon: <SiNextdotjs />, name: 'Next.js', color: '#000000' },
-
-    { icon: <img src={antDesign} alt="Ant Design" className="skills__img-icon" />, name: 'Ant Design' },
+    { icon: <img src={antDesign} alt="Ant Design" />, name: 'Ant Design' },
     { icon: <SiStyledcomponents />, name: 'Styled Components', color: '#DB7093' },
     { icon: <SiBootstrap />, name: 'Bootstrap', color: '#563d7c' },
-
     { icon: <SiGit />, name: 'Git', color: '#F05032' },
     { icon: <SiPostman />, name: 'Postman', color: '#FF6C37' },
     { icon: <SiJira />, name: 'Jira (Agile/Scrum)', color: '#0052CC' },
     { icon: <SiAsana />, name: 'Asana (Agile/Scrum)', color: '#FC636B' },
     { icon: <SiTrello />, name: 'Trello', color: '#0079BF' },
-    { icon: <img src={figma} alt="Figma" className="skills__img-icon" />, name: 'Figma' },
-
+    { icon: <img src={figma} alt="Figma" />, name: 'Figma' },
     { icon: <SiJest />, name: 'Jest', color: '#C21325' },
     { icon: <SiTestinglibrary />, name: 'React Testing Library', color: '#E33332' },
-
     { icon: <SiSocketdotio />, name: 'WebSockets (Socket.io)', color: '#010101' },
     { icon: <SiGithub />, name: 'GitHub', color: '#181717' },
-    { icon: <img src={gitlab} alt="GitLab" className="skills__img-icon" />, name: 'GitLab' },
-    { icon: <img src={bitbucket} alt="Bitbucket" className="skills__img-icon" />, name: 'Bitbucket' },
+    { icon: <img src={gitlab} alt="GitLab" />, name: 'GitLab' },
+    { icon: <img src={bitbucket} alt="Bitbucket" />, name: 'Bitbucket' },
 ];
 
-const rowCounts = [7, 8, 7];
-
-const Skills = () => {
-    let start = 0;
-    return (
-        <section id="experience" className="skills">
-            <h5>What Skills I Have</h5>
-            <h2>My Experience</h2>
-            <div className="skills__content container">
-                <div className="skills__circle">
-                    {rowCounts.map((count, row) => {
-                        const slice = skills.slice(start, start + count);
-                        start += count;
-                        return (
-                            <div key={row} className="skills__row">
-                                {slice.map(s => (
-                                    <div
-                                        key={s.name}
-                                        className="skills__icon"
-                                        title={s.name}
-                                        style={{ color: s.color }}
-                                    >
-                                        {s.icon}
-                                    </div>
-                                ))}
-                            </div>
-                        );
-                    })}
-                </div>
+const Skills = () => (
+    <section id="experience" className="skills">
+        <h5>What Skills I Have</h5>
+        <h2>My Experience</h2>
+        <div className="skills__content container">
+            <div className="skills__grid">
+                {skills.map((s) => (
+                    <div key={s.name} className="skills__icon" style={{ color: s.color || '#fff' }}>
+                        {s.icon}
+                        <span className="skills__tooltip">{s.name}</span>
+                    </div>
+                ))}
             </div>
-        </section>
-    );
-};
+        </div>
+    </section>
+);
 
 export default Skills;
